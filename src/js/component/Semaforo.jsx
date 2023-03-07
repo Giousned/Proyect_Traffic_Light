8c4e34e3e226209591d3147e53740e6c4137402f
Giousned <guille_ranza@hotmail.com>
2023-03-07 17:47:40 +0000

    [FIX] changing colors and stop function + clearTimeout

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useEffect, useState } from "react";
 
 import "../../styles/semaforo.css";
 
@@ -21,35 +20,7 @@ const Semaforo = (props) => {
 
 export default Semaforo;
 
-// {(props.contador) ? changeColor() : null}
-
-// n=0;
-// 	x = setInterval(function(){
-// 		console.log(n);
-// 		if(n==100){ clearInterval(x); }
-// 		n++;
-// 	},1000);
-
-// useEffect(() => {
-//   if (props.extra === true){
-//     for(let i=0; i<13; i++){
-//       setTimeout(() => {
-//         if (i === 0 || i === 4 || i === 8) setColor(RED);
-//         else if (i === 1 || i === 5 || i === 9) setColor(YELLOW);
-//         else if (i === 2 || i === 6 || i === 10) setColor(GREEN);
-//         else if (i === 3 || i === 7 || i === 11) setColor(PURPLE);
-//         else if (i === 12) setColor("");
-//         }, 1300*i);}
-//   }
-//   else for(let i=0; i<10; i++){
-//     setTimeout(() => {
-//       if (i === 0 || i === 3 || i === 6) setColor(RED);
-//       else if (i === 1 || i === 4 || i === 7) setColor(YELLOW);
-//       else if (i === 2 || i === 5 || i === 8) setColor(GREEN);
-//       else if (i === 9) setColor("");
-//       }, 1300 * i);}
-// },[props.contador]);
-
+// ASÍ ES COMO SE HACE REPITIENDO CODIGO Y SIN MAPEAR Y SIN FACTORIZAR
 // <div onClick={() => {setColor(RED);}} className={"trafficLight lightRed" + ((color === RED) ? " glow" : "")}></div>
 // <div onClick={() => {setColor(YELLOW);}} className={"trafficLight lightYellow" + ((color === YELLOW) ? " glow" : "")}></div>
 // <div onClick={() => {setColor(GREEN);}} className={"trafficLight lightGreen" + ((color === GREEN) ? " glow" : "")}></div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,6 +5,7 @@ import Buttons from "./Buttons.jsx";
 
 import { RED, YELLOW, GREEN, PURPLE, lightColors } from "../component/config.jsx"
 
+
 //create your first component
 const Home = () => {
 	const [extra, setExtra] = useState(lightColors);
@@ -12,36 +13,37 @@ const Home = () => {
 	const [changeColor, setChangeColor] = useState(false);
 	const [color, setColor] = useState("");
 
-	if(changeColor) {
-		setTimeout(() => {
-			if(!changeColor) return;
-			setColor(prev => {
-				if(!changeColor) return "";
-				const indicePrev = extra.indexOf(prev);
-				if(extra.length === (indicePrev + 1) || indicePrev === -1) {
-					return extra[0];
-				}
-				return extra[indicePrev+1]
-			})
-		},1500)
+	let timer = 0;
+
+	function cambiarColor () {
+		setColor((prev) => {
+			const indicePrev = extra.indexOf(prev);
+			if (extra.length === (indicePrev + 1) || indicePrev === -1) {
+				return extra[0];
+			}
+			return extra[indicePrev + 1]
+		})
 	}
 
-	console.log(changeColor);
-	console.log(color);
+	if (changeColor) timer = setTimeout(cambiarColor,1500);
 
+	// console.log(changeColor);
+	// console.log(color);
 
 	const handleSwicthOFF = () => {
 		setChangeColor(false);
 		setColor("");
+		clearTimeout(timer);
 	}
 
 	const handleColorChange = () => {
+		setColor(RED);
 		setChangeColor(!changeColor);
   	}
 
 	const handleAddExtra = () => {
 		if(extra.includes(PURPLE)) setExtra([...lightColors])
-		else setExtra([...lightColors,PURPLE]);
+		else setExtra([...lightColors, PURPLE]);
   	}
 
 	return (
@@ -53,3 +55,30 @@ const Home = () => {
 };
 
 export default Home;
+
+// n=0;
+// 	x = setInterval(function(){
+// 		console.log(n);
+// 		if(n==100){ clearInterval(x); }
+// 		n++;
+// 	},1000);
+
+// useEffect(() => {
+//   if (props.extra === true){
+//     for(let i=0; i<13; i++){
+//       setTimeout(() => {
+//         if (i === 0 || i === 4 || i === 8) setColor(RED);
+//         else if (i === 1 || i === 5 || i === 9) setColor(YELLOW);
+//         else if (i === 2 || i === 6 || i === 10) setColor(GREEN);
+//         else if (i === 3 || i === 7 || i === 11) setColor(PURPLE);
+//         else if (i === 12) setColor("");
+//         }, 1300*i);}
+//   }
+//   else for(let i=0; i<10; i++){
+//     setTimeout(() => {
+//       if (i === 0 || i === 3 || i === 6) setColor(RED);
+//       else if (i === 1 || i === 4 || i === 7) setColor(YELLOW);
+//       else if (i === 2 || i === 5 || i === 8) setColor(GREEN);
+//       else if (i === 9) setColor("");
+//       }, 1300 * i);}
+// },[props.contador]);
